ee2ec9dd9680d91b8a5ce3c261912028b8279513
gustavcaves <rungustavo@gmail.com>
2021-03-14 23:38:01 +0100

    creating CreateView

--- web_playground/pages/urls.py
+++ web_playground/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+pages_patterns = ([
     path('', views.PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', views.PageDetailView.as_view(), name='page'),
-]
+    path('create/', views.PageCreate.as_view(), name='create'),
+], 'pages')
--- web_playground/pages/views.py
+++ web_playground/pages/views.py
@@ -4,6 +4,10 @@ from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+from django.views.generic.edit import CreateView
+
+from django.urls import reverse, reverse_lazy
+
 # Create your views here.
 # def pages(request):
 #     pages = get_list_or_404(Page)
@@ -20,3 +24,16 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
 
     model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+    # success_url = reversed('pages:page') | THIS IS NOT THE WAY
+
+    # def get_success_url(self) -> str: | THIS IS THE ORIGINAL FUNTION
+    #     return super().get_success_url()
+
+    # def get_success_url(self): # THIS IS A WAY TO DO IT | THIS REDIRECT THE PAGE
+    #     return reverse('pages:pages')
+    # BUT OVERWRITE THE METHOD get IS TEDIOUS IT NOT HAVE SENSE SO USE reverse_laze import it
